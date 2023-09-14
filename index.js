194e49d48b1dd8e5e8b3e31a94c99b30b767a0a8
bbeeken <41312445+bbeeken@users.noreply.github.com>
2023-09-14 10:52:45 -0500

    ok

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const excel = require("exceljs");
+
+const app = express();
+const port = 3000;
+
+const upload = multer({ dest: "uploads/" });
+
+app.post("/convert", upload.single("file"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send({ error: "No file uploaded" });
+    }
+
+    const csvOutput = await convertExcelToCSV(req.file.path);
+
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader("Content-Disposition", "attachment; filename=output.csv");
+    res.send(csvOutput);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  } finally {
+    // Clean up the uploaded file
+    fs.unlinkSync(req.file.path);
+  }
+});
+
+async function convertExcelToCSV(filePath) {
+  const workbook = new excel.Workbook();
+  await workbook.xlsx.readFile(filePath);
+
+  let csvOutput = "";
+
+  workbook.worksheets.forEach((worksheet) => {
+    let rowCount = 0;
+
+    worksheet.eachRow({ includeEmpty: false }, (row) => {
+      rowCount++;
+
+      if (rowCount <= 2) {
+        return; // skip the header and the second row
+      }
+
+      const firstCellValue = row.getCell(1).value;
+
+      if (
+        firstCellValue &&
+        !isNaN(firstCellValue) &&
+        typeof firstCellValue !== "object"
+      ) {
+        csvOutput +=
+          row.values
+            .slice(1, 8) // Only use the first 7 columns
+            .map((cell) => {
+              if (cell && typeof cell === "object") {
+                if (cell.richText) {
+                  // Concatenate rich text segments and remove commas
+                  return cell.richText
+                    .map((segment) => segment.text.replace(/,/g, ""))
+                    .join("");
+                }
+                // Attempt to convert other types of objects
+                return JSON.stringify(cell);
+              }
+              return String(cell).replace(/,/g, ""); // Remove commas
+            })
+            .join(",") + "\n";
+      }
+    });
+  });
+
+  return csvOutput;
+}
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
