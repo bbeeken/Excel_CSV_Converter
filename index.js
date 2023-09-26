4ca51b0084a44d0f3a368400d3327cd0f45763d1
bbeeken <41312445+bbeeken@users.noreply.github.com>
2023-09-26 13:22:41 -0500

    ok

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const multer = require("multer");
 const fs = require("fs");
 const excel = require("exceljs");
@@ -6,8 +7,54 @@ const excel = require("exceljs");
 const app = express();
 const port = 3000;
 
+// Middleware for JSON Base64 uploads
+app.use(bodyParser.json({ limit: "50mb" }));
+
+// Middleware for multipart/form-data uploads
 const upload = multer({ dest: "uploads/" });
 
+// Endpoint for JSON Base64 uploads
+// Endpoint for JSON Base64 uploads
+app.post("/convert-base64", async (req, res) => {
+  const { fileName, fileContent } = req.body;
+
+  if (!fileName || !fileContent) {
+    return res.status(400).json({ error: "Invalid parameters" });
+  }
+
+  // Decode the Base64 encoded file
+  const buffer = Buffer.from(fileContent, "base64");
+
+  // Temporary file path
+  const tempFilePath = `uploads/${fileName}`;
+
+  // Save the buffer as a file
+  fs.writeFile(tempFilePath, buffer, async (err) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to write file" });
+    }
+
+    try {
+      // Convert the Excel file to CSV
+      const csvOutput = await convertExcelToCSV(tempFilePath);
+
+      // Set headers and send the CSV output
+      res.setHeader("Content-Type", "text/csv");
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=${fileName}.csv`
+      );
+      res.send(csvOutput);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    } finally {
+      // Clean up the temporary file
+      fs.unlinkSync(tempFilePath);
+    }
+  });
+});
+
+// Endpoint for multipart/form-data uploads
 app.post("/convert", upload.single("file"), async (req, res) => {
   try {
     if (!req.file) {
